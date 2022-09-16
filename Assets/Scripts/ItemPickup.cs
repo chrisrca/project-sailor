cfa0dd031d9c7653492416b12d246123adf5e1d1
Ikhung25 <73298612+Ikhung25@users.noreply.github.com>
2022-09-16 11:23:23 -0400

    Add files via upload

--- Assets/Scripts/ItemPickup.cs
+++ Assets/Scripts/ItemPickup.cs
@@ -12,23 +12,30 @@ public class ItemPickup : MonoBehaviour
     }
 
     public ItemType type;
+    public AudioSource appear;
+    public AudioClip pickUp;
 
     private void OnItemPickup(GameObject player)
     {
+        
         switch (type)
         {
             case ItemType.ExtraBomb:
                 player.GetComponent<BombController>().AddBomb();
+                appear.Play();
                 break;
 
             case ItemType.BlastRadius:
                 player.GetComponent<BombController>().explosionRadius++;
+                appear.Play();
                 break;
 
             case ItemType.SpeedIncrease:
                 player.GetComponent<MovementController>().speed++;
+                appear.Play();
                 break;
         }
+        AudioSource.PlayClipAtPoint(pickUp, transform.position);
         Destroy(gameObject);
     }
 

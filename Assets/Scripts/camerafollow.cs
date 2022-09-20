14f0ac327795648752866f5c0118db6a8dda02e9
chrisrca <goalie.cyclone@gmail.com>
2022-09-20 16:07:31 -0400

    fixed branch merge error

--- Assets/Scripts/camerafollow.cs
+++ Assets/Scripts/camerafollow.cs
@@ -16,7 +16,7 @@ public class camerafollow : MonoBehaviour
 
     void Start() 
     {
-        mainCamera.transform.position = new Vector3(-6f, 5f, -10f);
+        mainCamera.transform.position = camcoords;
     }
 
     void Update()
